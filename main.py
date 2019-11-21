275177c1002a303b29a2936123c3c26a2bbea006
s172189 <s172189@student.dtu.dk>
2019-11-21 13:57:50 +0100

    fixed inputs

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
-from src import utils
-
+import numpy as np
 import pandas as pd
 
 def getHeroesInMatch(id):
-    return 0
+    return players.loc[players['match_id'] == id]
 
+def getHeroesInTeam(heroes, slots):
+    return heroes.loc[(heroes['player_slot'] >= slots[0]) & (heroes['player_slot'] <= slots[1])]
+
+def generateInputArray(playersInTeam):
+    array = np.empty(len(heroes))
+    array.fill(0)
+
+    for i in range(len(playersInTeam)):
+        hero = players.loc[i]['hero_id']
+        array[hero - 1] = 1
+
+    return array
+
+
+#Init variables
+direSlots = [128,132]
+radiantSlots = [0,4]
 path = "../dota_dataset/"
-
+#Fetch relevant data
 matches = pd.read_csv(path + "match.csv")
 players = pd.read_csv(path + "players.csv")
 heroes = pd.read_csv(path + "hero_names.csv")['hero_id'].values
 
-print(len(heroes))
 
 
-heroList = heroes['hero_id']
-
-print(heroList)
-
-#utils.defineInputs(matches.loc[0], heroList)
+playersInMatch = getHeroesInMatch(0)
 
 
+radiantArray = generateInputArray(getHeroesInTeam(playersInMatch, radiantSlots))
+direArray = generateInputArray(getHeroesInTeam(playersInMatch, direSlots))
+
+print(radiantArray)
+print(direArray)
 
 
-
-print("hello world")
 
--- utils.py
+++ utils.py
@@ -1,14 +1,3 @@
 import pandas as pd
 import numpy as np
 
-
-
-def defineInputs(players, heroList):
-    array = np.empty(len(heroList))
-    array.fill(0)
-
-    for i in range(len(players)):
-        print(i)
-
-
-    return array
